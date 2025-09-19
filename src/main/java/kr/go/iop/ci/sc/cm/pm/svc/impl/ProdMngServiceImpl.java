07277da054f157a4f36fd897ed24fafbe57673d3
jh.park <jh.park@msa.com>
2025-09-19 11:06:07 +0900

    수정

--- src/main/java/kr/go/iop/ci/sc/cm/pm/svc/impl/ProdMngServiceImpl.java
+++ src/main/java/kr/go/iop/ci/sc/cm/pm/svc/impl/ProdMngServiceImpl.java
@@ -64,6 +64,7 @@ public class ProdMngServiceImpl implements ProdMngService {
         int resultCnt = prodMngMapper.insertProd(vo);
         if (resultCnt <= 0) throw new RuntimeException("상품 등록 실패");
         vo.setApiVerSn(vo.getSaasPrdctApiVerSn());
+        vo.setSrvrSeCd(ConstantInfo.TEST_OPS);
 
         List<ProdMngDVO> apiList = selectStdApiInfo(vo);
         for (ProdMngDVO apiVo : apiList) {
@@ -71,21 +72,16 @@ public class ProdMngServiceImpl implements ProdMngService {
             insertVo.setSaasPrdctId(vo.getSaasPrdctId());
             insertVo.setApiVerSn(vo.getApiVerSn());
             insertVo.setApiId(apiVo.getApiId());
-            insertVo.setSrvrSeCd(ConstantInfo.TEST_DEV);
+            insertVo.setSrvrSeCd(vo.getSrvrSeCd());
             insertVo.setCmncRsltCd(ConstantInfo.TEST_WAIT);
             insertVo.setFrstCrtPrcrId(vo.getFrstCrtPrcrId());
             insertVo.setLastChgPrcrId(vo.getLastChgPrcrId());
 
             insertStdApi(insertVo);
             insertProdApiTest(insertVo);
-
-            insertVo.setSrvrSeCd(ConstantInfo.TEST_OPS);
-            insertProdApiTest(insertVo);
         }
         
-        vo.setSrvrSeCd(ConstantInfo.TEST_DEV);
-        insertCertKey(vo);
-        vo.setSrvrSeCd(ConstantInfo.TEST_OPS);
+        
         insertCertKey(vo);
         
         return resultCnt;

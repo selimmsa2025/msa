5593a31d6645b850e659ccbb989042ccc2bf2f2e
sy.park <MSA.com>
2025-09-22 16:59:15 +0900

    제품 상세 쿼리 수정

--- src/main/java/kr/go/iop/ci/sc/cp/cpm/mapper/vo/ProdDVO.java
+++ src/main/java/kr/go/iop/ci/sc/cp/cpm/mapper/vo/ProdDVO.java
@@ -116,6 +116,8 @@ public class ProdDVO implements Serializable {
 	
 	
 	/* 상품 코드명칭 반환 e */
+	@Schema(description = "제품구독상태코드")
+	private String 	prdsbscSttsCd;
 
 	/* 상품 서비스 반환 s */
 	@Schema(description = "서비스아이디")
